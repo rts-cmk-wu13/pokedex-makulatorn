a0be51f62e25f73321238fc75f8c2105df7875c8
sasha <sasha.friis@icloud.com>
2025-02-24 13:48:08 +0100

    added font, more styling + updated JS

--- js/detail.js
+++ js/detail.js
@@ -4,7 +4,7 @@ const pokemonName = urlParams.get('name');
 fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(response => response.json())
     .then(pokemon => {
-        const rootElement = document.querySelector("#root");
+        const mainElement = document.querySelector("main");  // Select the existing <main> element
 
         document.title = `${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} - Pokémon`;
 
@@ -12,33 +12,35 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
         const pokemonHtml = `
         <section class="pokemon-detail">
-        <span>
-        <h1>${pokemon.name}</h1>
-        <p>${formattedId}</p>
-        </span>
-                <div class="pokemon-img-con">
-                    <img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}" />
-                </div>
-                <div class="pokemon-txt-con">
-                    <h3 class="pokemon-types">Types:</h3>
-                    <ul class="pokemon-types-list">
-                        ${pokemon.types.map(type => `<li>${type.type.name}</li>`).join('')}
-                    </ul>
-                    <h3 class="pokemon-abilities">Abilities:</h3>
-                    <ul class="pokemon-abilities-list">
-                        ${pokemon.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
-                    </ul>
-                    <h3 class="pokemon-stats">Stats:</h3>
-                    <ul class="pokemon-stats-list">
-                        ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
-                    </ul>
-                </div>
-            </section>
+            <span>
+                <h1>${pokemon.name}</h1>
+                <p>${formattedId}</p>
+            </span>
+            <div class="pokemon-img-con">
+                <img class="pokemon-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="${pokemon.name}" />
+            </div>
+            <div class="pokemon-txt-con">
+                <h3 class="pokemon-types">Types:</h3>
+                <ul class="pokemon-types-list">
+                    ${pokemon.types.map(type => `<li>${type.type.name}</li>`).join('')}
+                </ul>
+                <h3 class="pokemon-abilities">Abilities:</h3>
+                <ul class="pokemon-abilities-list">
+                    ${pokemon.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
+                </ul>
+                <h3 class="pokemon-stats">Stats:</h3>
+                <ul class="pokemon-stats-list">
+                    ${pokemon.stats.map(stat => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+                </ul>
+            </div>
+        </section>
         `;
 
-        rootElement.innerHTML = pokemonHtml;
+        // Insert the generated HTML into the existing <main> element
+        mainElement.innerHTML = pokemonHtml;
     })
     .catch(error => {
         console.error("Error fetching Pokémon data:", error);
-        document.querySelector("#root").innerHTML = "<p>Sorry, Pokémon details could not be found.</p>";
+        const mainElement = document.querySelector("main");
+        mainElement.innerHTML = "<p>Sorry, Pokémon details could not be found.</p>";
     });
--- js/home.js
+++ js/home.js
@@ -3,31 +3,98 @@
  * @returns {string}
  */
 function getIdFromPokemon(pokemonUrl) {
-    return pokemonUrl.slice(0, -1).split("/").pop()
+    return pokemonUrl.slice(0, -1).split("/").pop();
 }
 
-const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork"
+const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork";
 
-let sectionElm = document.createElement("section")
-sectionElm.className = "pokelist"
+let sectionElm = document.createElement("section");
+sectionElm.className = "pokelist";let allpokemon = [];
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+const fetchPokemon = () => {
+
+    //showLoader(true);
+
+    const promises = [];
+
+    for (let i = 1; i <= 48; i++) {
+
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then((results) => {
+
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites.other['official-artwork'].front_default,
+            type: result.types.map((type) => type.type.name),
+            id: result.id
+        })).sort((a, b) => a.id > b.id ? 1 : -1);
+
+        populateArray(pokemon);
+
+        //showLoader(false);
+
+        appendToMain(pokemon);
+    });
+};
+
+function populateArray(pokeArray) {
+    pokeArray.forEach((pokemon) => {
+        allpokemon.push(pokemon);
+    });
+    console.log(allpokemon);
+}
+
+function appendToMain(pokeArray) {
+    let sectionElm = document.createElement("section");
+    sectionElm.className = "pokelist";
+
+    sectionElm.innerHTML = pokeArray.map(pokemon => {
+        const formattedId = `#${String(pokemon.id).padStart(3, '0')}`; // Add the formatted ID here
+        return `
+            <article class="pokecard">
+                <a class="pokecard-name" href="detail.html?name=${pokemon.name}" style="font-size:80%;">
+                    <p class="pokecard-id">${formattedId}</p>
+                    <img src="${pokemon.image}" alt="Official artwork of ${pokemon.name}">
+                    ${pokemon.name}
+                </a>
+            </article>
+        `;
+    }).join("");
+
+    document.querySelector("main").append(sectionElm);
+};
+
+function getCSScolor(varName) {
+    const root = document.documentElement;
+    const currentColor = getComputedStyle(root).getPropertyValue(varName);
+    return currentColor;
+}
+ 
+
+fetchPokemon();
+
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=21")
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         sectionElm.innerHTML = data.results.map(pokemon => {
             const pokemonId = getIdFromPokemon(pokemon.url);
+            const formattedId = `#${String(pokemonId).padStart(3, '0')}`; // Add the formatted ID here
             return `
                 <article class="pokecard">
-                <img src="${artworkUrl}/${pokemonId}.png" alt="${pokemon.name}">
-                <a href="detail.html?name=${pokemon.name}">
+                <a class="pokecard-name" href="detail.html?name=${pokemon.name}" style="font-size:80%;">
+                    <p class="pokecard-id">${formattedId}</p>
+                    <img src="${artworkUrl}/${pokemonId}.png" alt="Official artwork of ${pokemon.name}">
                     ${pokemon.name}</a>
                 </article>
             `;
         }).join("");
     });
-
 
 // Append the section element to the main element
 document.querySelector("main").append(sectionElm);
